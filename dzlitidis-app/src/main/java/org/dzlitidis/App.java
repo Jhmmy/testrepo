fa4b0c0229d4914b611aaa47d4df81934519f997
Dimitrios Zlitidis <zlitiddi@aris-lux.lan>
2017-05-10 12:57:40 +0300

    New Commit

--- dzlitidis-app/src/main/java/org/dzlitidis/App.java
+++ dzlitidis-app/src/main/java/org/dzlitidis/App.java
@@ -4,10 +4,31 @@ package org.dzlitidis;
  * Hello world!
  *
  */
+/**
+ * @author zlitiddi
+ *
+ */
 public class App 
 {
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+    
+    /** 
+     * Test method
+     *  */
+    private static void test(){
+    	
+    }
+    
+    
+    /**
+     * @param test
+     * @return
+     */
+    private static String testString(String test){
+    	return test;
     }
 }
