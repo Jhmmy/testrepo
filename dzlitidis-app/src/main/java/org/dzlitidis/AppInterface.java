745a1f4bb7cea5505c66a3b3468d7f035d3d8307
dzlitidis <Dimitrios.Zlitidis@arhs-dev-hellas.com>
2017-05-10 15:56:46 +0300

    "dasljkdfnas"

---
+++ dzlitidis-app/src/main/java/org/dzlitidis/AppInterface.java
@@ -0,0 +1,13 @@
+package org.dzlitidis;
+
+/**
+ * @author zlitiddi
+ *
+ */
+public interface AppInterface {
+	
+	/**
+	 * @return
+	 */
+	String returnVersion();
+}
---
+++ dzlitidis-app/src/main/java/org/dzlitidis/AppInterfaceImpl.java
@@ -0,0 +1,17 @@
+package org.dzlitidis;
+
+/**
+ * @author zlitiddi
+ *
+ */
+public class AppInterfaceImpl implements AppInterface {
+
+	
+	/* (non-Javadoc)
+	 * @see org.dzlitidis.AppInterface#returnVersion()
+	 */
+	public String returnVersion() {
+		return "0.0.1-SNAPSHOT";
+	}
+
+}
